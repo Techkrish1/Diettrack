df554b244fa9583609c431161e5b89503c1cf031
gokul <ghasy79>
2023-08-15 12:59:01 +0530

    formatted

--- tracker/views.py
+++ tracker/views.py
@@ -4,11 +4,13 @@ def home(request):
 
     import json
     import requests
-    
+
+    # Nutrition api linked here
+
     if request.method == 'POST':
         query =request.POST['query']
         api_url = 'https://api.api-ninjas.com/v1/nutrition?query=' # this is the api url of Nutrition
-        api_request = requests.get(api_url + query, headers= {'X-Api-Key': 'hP8V6fK4YQNKyDlg/QoYdg==1ni2XZJ9Vve4fWsD'}) # this is my api key for api-ninjas website
+        api_request = requests.get(api_url + query, headers= {'X-Api-Key': 'hP8V6fK4YQNKyDlg/QoYdg==1ni2XZJ9Vve4fWsD'})
         try:
             api = json.loads(api_request.content)
         except Exception as e:
